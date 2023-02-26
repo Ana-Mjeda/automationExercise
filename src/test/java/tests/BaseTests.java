7fd8fc2ba9817c725ecff3bdde73cd185a39b2e3
ana.mjeda <anamjeda@gmail.com>
2023-02-26 20:11:17 +0100

    Initial commit

---
+++ src/test/java/tests/BaseTests.java
@@ -0,0 +1,34 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTests {
+    protected WebDriver driver;
+    protected WebDriverWait driverWait;
+    protected final String baseURL = "https://automationexercise.com";
+
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new ChromeDriver();
+        driverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.get(baseURL);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
